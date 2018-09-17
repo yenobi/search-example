fc27a6614677534727e68d2e25f18fc59bb6fd8d
yenobi <koshman.animys@gmail.com>
2018-09-17 09:07:34 +0300

    wire up search service with component via Observable stream and async pipe in template, add markup to show results, add basic styles

--- src/app/ff-test-page/ff-test-page.component.ts
+++ src/app/ff-test-page/ff-test-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FfTestPageService} from '@app/ff-test-page/ff-test-page.service';
+import {Joke} from '@app/ff-test-page/ff-test-page.models';
+import {Observable} from 'rxjs';
+import {map, tap} from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-ff-test-page',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ff-test-page.component.scss']
 })
 export class FfTestPageComponent implements OnInit {
+  public search = '';
+  public isLoading = false;
+  public result$: Observable<Joke>;
 
-  constructor() { }
+  constructor(
+    private _ff: FfTestPageService
+  ) { }
 
   ngOnInit() {
+  }
+
+  public submitSearch() {
+    this._toggleLoading();
+    this.result$ = this._ff
+      .searchJokes(this.search)
+      .pipe(
+        tap((res) => {
+          this._toggleLoading();
+          return res;
+        }),
+        map(({ result, total }) => (result)),
+      );
+  }
+
+  private _toggleLoading() {
+    this.isLoading = !this.isLoading;
   }
 
 }
